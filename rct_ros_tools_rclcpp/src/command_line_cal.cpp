35c47d3c6b58c3b7d3b901e5ff32d56f842cb51b
Michael Ripperger <michael.ripperger@swri.org>
2023-04-26 18:59:55 -0500

    Added rct_ros_tools_rclcpp

---
+++ rct_ros_tools_rclcpp/src/command_line_cal.cpp
@@ -0,0 +1,200 @@
+#include <rct_image_tools/data_set.h>
+#include <rct_image_tools/parameter_loaders.h>
+#include <rct_image_tools/target_finder_plugin.h>
+
+#include <boost_plugin_loader/plugin_loader.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <std_srvs/srv/trigger.hpp>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <vector>
+#include <yaml-cpp/yaml.h>
+
+template <typename T>
+T declare_and_get(rclcpp::Node* node, const std::string& key)
+{
+  T val;
+  node->declare_parameter(key);
+  if (!node->get_parameter(key, val))
+    throw(std::runtime_error("Failed to load '" + key + "' parameter"));
+  return val;
+}
+
+class DataCollection : public rclcpp::Node
+{
+public:
+  explicit DataCollection()
+    : Node("command_line_cal_node")
+    , clock_(std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME))
+    , buffer_(clock_)
+    , listener_(buffer_)
+  {
+    base_frame_ = declare_and_get<std::string>(this, "base_frame");
+    tool_frame_ = declare_and_get<std::string>(this, "tool_frame");
+    image_topic_ = declare_and_get<std::string>(this, "image_topic");
+    save_dir_ = declare_and_get<std::string>(this, "save_dir");
+    auto target_file = declare_and_get<std::string>(this, "target_file");
+
+    it_sub_ = image_transport::create_subscription(
+        this, image_topic_, std::bind(&DataCollection::onNewImage, this, std::placeholders::_1), "raw");
+    it_pub_ = image_transport::create_publisher(this, image_topic_ + "_out");
+
+    trigger_server_ = create_service<std_srvs::srv::Trigger>(
+        "collect", std::bind(&DataCollection::onTrigger, this, std::placeholders::_1, std::placeholders::_2));
+    save_server_ = create_service<std_srvs::srv::Trigger>(
+        "save", std::bind(&DataCollection::onSave, this, std::placeholders::_1, std::placeholders::_2));
+
+    // Configure the plugin loader
+    loader_.search_libraries.insert(RCT_TARGET_PLUGINS);
+
+    // Load the target finder plugin
+    const YAML::Node target_config = YAML::LoadFile(target_file)["target_finder"];
+    finder_ = loader_.createInstance<rct_image_tools::TargetFinderPlugin>(target_config["type"].as<std::string>());
+    finder_->init(target_config);
+  }
+
+private:
+  void onNewImage(const sensor_msgs::msg::Image::ConstSharedPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      if (msg->encoding == "mono16")
+      {
+        cv_bridge::CvImagePtr temp_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO16);
+
+        cv::Mat img_conv;
+        cv::cvtColor(temp_ptr->image, img_conv, CV_GRAY2BGR);
+        img_conv.convertTo(img_conv, CV_8UC1);
+        cv_ptr = cv_bridge::CvImagePtr(
+            new cv_bridge::CvImage(temp_ptr->header, sensor_msgs::image_encodings::BGR8, img_conv));
+      }
+      else
+      {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      }
+      last_frame_ = cv_ptr;
+
+      try
+      {
+        cv::Mat tmp_image = cv_ptr->image.clone();
+        rct_image_tools::TargetFeatures image_observations = finder_->findTargetFeatures(tmp_image);
+        cv::Mat modified_image = finder_->drawTargetFeatures(tmp_image, image_observations);
+        cv_bridge::CvImagePtr ptr(new cv_bridge::CvImage(cv_ptr->header, cv_ptr->encoding, modified_image));
+        it_pub_.publish(ptr->toImageMsg());
+      }
+      catch (const std::runtime_error& ex)
+      {
+        RCLCPP_ERROR(get_logger(), ex.what());
+        it_pub_.publish(cv_ptr->toImageMsg());
+      }
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      RCLCPP_ERROR_STREAM(get_logger(), e.what());
+      return;
+    }
+  }
+
+  void onTrigger(std_srvs::srv::Trigger::Request::SharedPtr, std_srvs::srv::Trigger::Response::SharedPtr res)
+  {
+    RCLCPP_INFO_STREAM(get_logger(), "Pose/Image capture triggered...");
+    geometry_msgs::msg::TransformStamped pose;
+    cv::Mat image;
+
+    if (captureTransform(pose) && captureImage(image))
+    {
+      poses_.push_back(pose);
+      images_.push_back(image);
+      res->success = true;
+      res->message = "Data collected successfully";
+    }
+    else
+    {
+      res->success = false;
+      res->message = "Failed to capture pose/image pair";
+    }
+  }
+
+  void onSave(std_srvs::srv::Trigger::Request::SharedPtr, std_srvs::srv::Trigger::Response::SharedPtr res)
+  {
+    rct_ros_tools::ExtrinsicDataSet data;
+    for (std::size_t i = 0; i < poses_.size(); ++i)
+    {
+      cv::Mat image = images_[i];
+      auto msg = poses_[i];
+
+      Eigen::Isometry3d pose = tf2::transformToEigen(msg);
+      data.images.push_back(image);
+      data.tool_poses.push_back(pose);
+    }
+
+    res->success = rct_ros_tools::saveToDirectory(save_dir_, data);
+    res->message = res->success ? "Success" : "Failed to save data";
+  }
+
+  bool captureImage(cv::Mat& frame)
+  {
+    if (last_frame_)
+    {
+      frame = last_frame_->image;
+      return true;
+    }
+    return false;
+  }
+
+  bool captureTransform(geometry_msgs::msg::TransformStamped& out)
+  {
+    try
+    {
+      geometry_msgs::msg::TransformStamped t = buffer_.lookupTransform(base_frame_, tool_frame_, tf2::TimePointZero,
+                                                                       tf2::Duration(std::chrono::seconds(10)));
+      out = t;
+      return true;
+    }
+    catch (const tf2::TransformException& ex)
+    {
+      RCLCPP_WARN_STREAM(get_logger(), "Failed to compute transfrom between " << base_frame_ << " and " << tool_frame_
+                                                                              << ": " << ex.what());
+      return false;
+    }
+  }
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr trigger_server_;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr save_server_;
+
+  image_transport::Subscriber it_sub_;
+  image_transport::Publisher it_pub_;
+
+  std::string base_frame_;
+  std::string tool_frame_;
+  std::string image_topic_;
+  std::string save_dir_;
+
+  std::vector<geometry_msgs::msg::TransformStamped> poses_;
+  std::vector<cv::Mat> images_;
+
+  std::shared_ptr<rclcpp::Clock> clock_;
+  tf2_ros::Buffer buffer_;
+  tf2_ros::TransformListener listener_;
+
+  cv_bridge::CvImagePtr last_frame_;
+
+  // Target finder plugin
+  boost_plugin_loader::PluginLoader loader_;
+  rct_image_tools::TargetFinderPlugin::Ptr finder_;
+};
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<DataCollection>();
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ rct_ros_tools_rclcpp/src/hand_eye_calibration.cpp
@@ -0,0 +1,180 @@
+// Utilities for loading data sets and calib parameters from YAML files via ROS
+#include <rct_common/print_utils.h>
+#include <rct_image_tools/data_set.h>
+#include <rct_image_tools/parameter_loaders.h>
+#include <rct_image_tools/target_finder_plugin.h>
+// The calibration function for 'moving camera' on robot wrist
+#include <rct_optimizations/extrinsic_hand_eye.h>
+#include <rct_optimizations/serialization/problems.h>
+#include <rct_optimizations/validation/homography_validation.h>
+// Calibration analysis
+#include "hand_eye_calibration_analysis.h"
+
+#include <boost_plugin_loader/plugin_loader.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_eigen/tf2_eigen.h>
+
+using namespace rct_optimizations;
+using namespace rct_image_tools;
+using namespace rct_ros_tools;
+using namespace rct_common;
+
+const std::string WINDOW = "window";
+
+template <typename T>
+T declare_and_get(rclcpp::Node* node, const std::string& key)
+{
+  T val;
+  node->declare_parameter(key);
+  if (!node->get_parameter(key, val))
+    throw(std::runtime_error("Failed to load '" + key + "' parameter"));
+  return val;
+}
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<rclcpp::Node>("camera_on_wrist_extrinsic");
+
+  // Load the data set path from ROS param
+  try
+  {
+    auto homography_threshold = declare_and_get<double>(node.get(), "homography_threshold");
+    CameraIntrinsics intr = loadIntrinsics(declare_and_get<std::string>(node.get(), "intrinsics"));
+
+    // Attempt to load the data set via the data record yaml file:
+    auto data_path = declare_and_get<std::string>(node.get(), "data_path");
+    boost::optional<ExtrinsicDataSet> maybe_data_set = parseFromFile(data_path);
+    if (!maybe_data_set)
+      throw std::runtime_error("Failed to parse data set from path = " + data_path);
+
+    // We know it exists, so define a helpful alias
+    const ExtrinsicDataSet& data_set = *maybe_data_set;
+
+    // Lets create a class that will search for the target in our raw images.
+    auto target_file = declare_and_get<std::string>(node.get(), "target");
+
+    // Configure the plugin loader
+    boost_plugin_loader::PluginLoader loader_;
+    loader_.search_libraries.insert(RCT_TARGET_PLUGINS);
+
+    // Load the target finder plugin
+    const YAML::Node target_config = YAML::LoadFile(target_file)["target_finder"];
+    TargetFinderPlugin::Ptr target_finder =
+        loader_.createInstance<rct_image_tools::TargetFinderPlugin>(target_config["type"].as<std::string>());
+    target_finder->init(target_config);
+
+    // Now we create our calibration problem
+    ExtrinsicHandEyeProblem2D3D problem;
+    problem.intr = intr;  // Set the camera properties
+
+    // Our 'base to camera guess': A camera off to the side, looking at a point centered in front of the robot
+    {
+      tf2_ros::Buffer buffer(node->get_clock());
+      tf2_ros::TransformListener listener(buffer);
+
+      auto target_mount_frame = declare_and_get<std::string>(node.get(), "target_mount_frame");
+      auto target_frame = declare_and_get<std::string>(node.get(), "target_frame");
+      auto camera_mount_frame = declare_and_get<std::string>(node.get(), "camera_mount_frame");
+      auto camera_frame = declare_and_get<std::string>(node.get(), "camera_frame");
+
+      tf2::Duration timeout(std::chrono::seconds(5));
+      problem.target_mount_to_target_guess =
+          tf2::transformToEigen(buffer.lookupTransform(target_mount_frame, target_frame, tf2::TimePointZero, timeout));
+      problem.camera_mount_to_camera_guess =
+          tf2::transformToEigen(buffer.lookupTransform(camera_mount_frame, camera_frame, tf2::TimePointZero, timeout));
+    }
+
+    // Create a named OpenCV window for viewing the images
+    cv::namedWindow(WINDOW, cv::WINDOW_NORMAL);
+
+    // Finally, we need to process our images into correspondence sets: for each dot in the
+    // target this will be where that dot is in the target and where it was seen in the image.
+    // Repeat for each image. We also tell where the wrist was when the image was taken.
+    problem.observations.reserve(data_set.images.size());
+
+    // The target may not be identified in all images, so let's keep track the indices of the images for which the
+    // target was identified
+    std::vector<cv::Mat> found_images;
+    found_images.reserve(data_set.images.size());
+
+    for (std::size_t i = 0; i < data_set.images.size(); ++i)
+    {
+      // For each image we need to:
+      //// 1. Try to find the target features in this image:
+      rct_image_tools::TargetFeatures target_features;
+      try
+      {
+        target_features = target_finder->findTargetFeatures(data_set.images[i]);
+        if (target_features.empty())
+          throw std::runtime_error("Failed to find any target features");
+        RCLCPP_INFO_STREAM(node->get_logger(), "Found " << target_features.size() << " target features");
+
+        Observation2D3D obs;
+        obs.correspondence_set.reserve(target_features.size());
+
+        //// 2. Record the wrist position and target features
+        obs.to_camera_mount = data_set.tool_poses[i];
+        obs.to_target_mount = Eigen::Isometry3d::Identity();
+        obs.correspondence_set = target_finder->target().createCorrespondences(target_features);
+
+        //// 3. Check that a homography matrix can accurately reproject the observed points onto the expected target points within a defined threshold
+        rct_optimizations::RandomCorrespondenceSampler random_sampler(obs.correspondence_set.size(),
+                                                                      obs.correspondence_set.size() / 3);
+        Eigen::VectorXd homography_error =
+            rct_optimizations::calculateHomographyError(obs.correspondence_set, random_sampler);
+        if (homography_error.array().mean() > homography_threshold)
+          throw std::runtime_error("Homography error exceeds threshold (" + std::to_string(homography_error.array().mean()) + ")");
+
+        //// 3. And finally add that to the problem
+        problem.observations.push_back(obs);
+        found_images.push_back(data_set.images[i]);
+
+        // Show the points we detected
+        cv::imshow(WINDOW, target_finder->drawTargetFeatures(data_set.images[i], target_features));
+        cv::waitKey();
+      }
+      catch (const std::runtime_error& ex)
+      {
+        RCLCPP_WARN_STREAM(node->get_logger(), "Image " << i << ": '" << ex.what() << "'");
+        cv::imshow(WINDOW, data_set.images[i]);
+        cv::waitKey();
+        continue;
+      }
+    }
+
+    // Now we have a defined problem, run optimization:
+    ExtrinsicHandEyeResult opt_result = optimize(problem);
+
+    // Report results
+    printOptResults(opt_result.converged, opt_result.initial_cost_per_obs, opt_result.final_cost_per_obs);
+    printNewLine();
+
+    Eigen::Isometry3d c = opt_result.camera_mount_to_camera;
+    printTransform(c, "Wrist", "Camera", "WRIST TO CAMERA");
+    printNewLine();
+
+    Eigen::Isometry3d t = opt_result.target_mount_to_target;
+    printTransform(t, "Base", "Target", "BASE TO TARGET");
+    printNewLine();
+
+    std::cout << opt_result.covariance.printCorrelationCoeffAboveThreshold(0.5) << std::endl;
+
+    // Now let's compare the results of our extrinsic calibration with a PnP optimization for every observation.
+    // The PnP optimization will give us an estimate of the camera to target transform using our input camera intrinsic
+    // parameters We will then see how much this transform differs from the same transform calculated using the results
+    // of the extrinsic calibration
+    analyzeResults(problem, opt_result, found_images, WINDOW);
+  }
+  catch (const std::exception& ex)
+  {
+    RCLCPP_ERROR_STREAM(node->get_logger(), ex.what());
+    return -1;
+  }
+
+  return 0;
+}
