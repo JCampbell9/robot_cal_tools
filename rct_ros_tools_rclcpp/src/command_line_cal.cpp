47af2f7089b8c98c898ef81e6d42741761e3909b
Michael Ripperger <michael.ripperger@swri.org>
2023-06-18 22:41:19 -0500

    Updated CLI cal to collect camera and target chain joints

--- rct_ros_tools_rclcpp/src/command_line_cal.cpp
+++ rct_ros_tools_rclcpp/src/command_line_cal.cpp
@@ -7,6 +7,7 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 #include <std_srvs/srv/trigger.hpp>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_eigen/tf2_eigen.h>
@@ -24,24 +25,69 @@ T declare_and_get(rclcpp::Node* node, const std::string& key)
   return val;
 }
 
+template <typename T>
+T getLatestMessage(typename rclcpp::Subscription<T>::SharedPtr sub)
+{
+  rclcpp::WaitSet wait;
+  wait.add_subscription(sub);
+  auto ret = wait.wait(std::chrono::seconds(3));
+  switch (ret.kind())
+  {
+    case rclcpp::WaitResultKind::Ready:
+      break;
+    default:
+      throw std::runtime_error("Failed to get message within timeout");
+  }
+
+  T msg;
+  rclcpp::MessageInfo info;
+  if (!sub->take(msg, info))
+  {
+    std::stringstream ss;
+    ss << "Failed to get latest message on topic '" << sub->get_topic_name() << "'";
+    throw std::runtime_error(ss.str());
+  }
+  return msg;
+}
+
+std::vector<double> getJointValues(const sensor_msgs::msg::JointState& msg, const std::vector<std::string>& joint_names)
+{
+  std::vector<double> values(joint_names.size());
+  for (const std::string& name : joint_names)
+  {
+    auto it = std::find(msg.name.begin(), msg.name.end(), name);
+    if (it == msg.name.end())
+      throw std::runtime_error("Failed to find joint '" + name + "' in joint state message");
+
+    auto idx = std::distance(msg.name.begin(), it);
+    values.push_back(msg.position.at(idx));
+  }
+
+  return values;
+}
+
 class DataCollection : public rclcpp::Node
 {
 public:
   explicit DataCollection()
     : Node("command_line_cal_node")
+    , base_frame_(declare_and_get<std::string>(this, "base_frame"))
+    , tool_frame_(declare_and_get<std::string>(this, "tool_frame"))
+    , image_topic_(declare_and_get<std::string>(this, "image_topic"))
+    , save_dir_(declare_and_get<std::string>(this, "save_dir"))
+    , camera_chain_joints_(declare_and_get<std::vector<std::string>>(this, "camera_chain_joints"))
+    , target_chain_joints_(declare_and_get<std::vector<std::string>>(this, "target_chain_joints"))
     , clock_(std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME))
     , buffer_(clock_)
     , listener_(buffer_)
   {
-    base_frame_ = declare_and_get<std::string>(this, "base_frame");
-    tool_frame_ = declare_and_get<std::string>(this, "tool_frame");
-    image_topic_ = declare_and_get<std::string>(this, "image_topic");
-    save_dir_ = declare_and_get<std::string>(this, "save_dir");
     auto target_file = declare_and_get<std::string>(this, "target_file");
 
     it_sub_ = image_transport::create_subscription(
         this, image_topic_, std::bind(&DataCollection::onNewImage, this, std::placeholders::_1), "raw");
     it_pub_ = image_transport::create_publisher(this, image_topic_ + "_out");
+
+    joint_state_sub_ = create_subscription<sensor_msgs::msg::JointState>("joint_states", 1, [](const sensor_msgs::msg::JointState::SharedPtr) {});
 
     trigger_server_ = create_service<std_srvs::srv::Trigger>(
         "collect", std::bind(&DataCollection::onTrigger, this, std::placeholders::_1, std::placeholders::_2));
@@ -102,86 +148,74 @@ private:
 
   void onTrigger(std_srvs::srv::Trigger::Request::SharedPtr, std_srvs::srv::Trigger::Response::SharedPtr res)
   {
-    RCLCPP_INFO_STREAM(get_logger(), "Pose/Image capture triggered...");
-    geometry_msgs::msg::TransformStamped pose;
-    cv::Mat image;
-
-    if (captureTransform(pose) && captureImage(image))
+    try
     {
-      poses_.push_back(pose);
-      images_.push_back(image);
+      RCLCPP_INFO_STREAM(get_logger(), "Pose/Image capture triggered...");
+
+      // Wait for a new joint state message
+      auto js = getLatestMessage<sensor_msgs::msg::JointState>(joint_state_sub_);
+      std::vector<double> camera_joints = getJointValues(js, camera_chain_joints_);
+      std::vector<double> target_joints = getJointValues(js, target_chain_joints_);
+
+      // Check the latest image
+      if (!last_frame_)
+        throw std::runtime_error("No image yet acquired");
+
+      if ((rclcpp::Time(last_frame_->header.stamp) - rclcpp::Time(js.header.stamp)) > rclcpp::Duration(1))
+        throw std::runtime_error("Last image time stamp differs by more than 1 second from the last joint state "
+                                 "message");
+
+      // Look up the transform
+      geometry_msgs::msg::TransformStamped t = buffer_.lookupTransform(
+          base_frame_, tool_frame_, last_frame_->header.stamp, tf2::Duration(std::chrono::seconds(3)));
+
+      // Save the data
+      data_.images.push_back(last_frame_->image);
+      data_.tool_poses.push_back(tf2::transformToEigen(t));
+      data_.camera_chain_joints.push_back(camera_joints);
+      data_.target_chain_joints.push_back(target_joints);
+
       res->success = true;
       res->message = "Data collected successfully";
     }
-    else
+    catch (const std::exception& ex)
     {
+      res->message = ex.what();
       res->success = false;
-      res->message = "Failed to capture pose/image pair";
     }
   }
 
   void onSave(std_srvs::srv::Trigger::Request::SharedPtr, std_srvs::srv::Trigger::Response::SharedPtr res)
   {
-    rct_ros_tools::ExtrinsicDataSet data;
-    for (std::size_t i = 0; i < poses_.size(); ++i)
-    {
-      cv::Mat image = images_[i];
-      auto msg = poses_[i];
-
-      Eigen::Isometry3d pose = tf2::transformToEigen(msg);
-      data.images.push_back(image);
-      data.tool_poses.push_back(pose);
-    }
-
-    res->success = rct_ros_tools::saveToDirectory(save_dir_, data);
+    res->success = rct_ros_tools::saveToDirectory(save_dir_, data_);
     res->message = res->success ? "Success" : "Failed to save data";
   }
 
-  bool captureImage(cv::Mat& frame)
-  {
-    if (last_frame_)
-    {
-      frame = last_frame_->image;
-      return true;
-    }
-    return false;
-  }
+  // Parameters
+  const std::string base_frame_;
+  const std::string tool_frame_;
+  const std::string image_topic_;
+  const std::string save_dir_;
+  const std::vector<std::string> camera_chain_joints_;
+  const std::vector<std::string> target_chain_joints_;
 
-  bool captureTransform(geometry_msgs::msg::TransformStamped& out)
-  {
-    try
-    {
-      geometry_msgs::msg::TransformStamped t = buffer_.lookupTransform(base_frame_, tool_frame_, tf2::TimePointZero,
-                                                                       tf2::Duration(std::chrono::seconds(10)));
-      out = t;
-      return true;
-    }
-    catch (const tf2::TransformException& ex)
-    {
-      RCLCPP_WARN_STREAM(get_logger(), "Failed to compute transfrom between " << base_frame_ << " and " << tool_frame_
-                                                                              << ": " << ex.what());
-      return false;
-    }
-  }
-  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr trigger_server_;
-  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr save_server_;
+  // Data
+  cv_bridge::CvImagePtr last_frame_;
+  rct_ros_tools::ExtrinsicDataSet data_;
 
+  // Data subscribers
   image_transport::Subscriber it_sub_;
   image_transport::Publisher it_pub_;
-
-  std::string base_frame_;
-  std::string tool_frame_;
-  std::string image_topic_;
-  std::string save_dir_;
-
-  std::vector<geometry_msgs::msg::TransformStamped> poses_;
-  std::vector<cv::Mat> images_;
 
   std::shared_ptr<rclcpp::Clock> clock_;
   tf2_ros::Buffer buffer_;
   tf2_ros::TransformListener listener_;
 
-  cv_bridge::CvImagePtr last_frame_;
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_sub_;
+
+  // ROS interfaces
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr trigger_server_;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr save_server_;
 
   // Target finder plugin
   boost_plugin_loader::PluginLoader loader_;
